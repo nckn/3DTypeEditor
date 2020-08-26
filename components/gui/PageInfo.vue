dcc6a62107c9527cfdfce9074d391b74e062df51
Niels Christian Konrad Nielsen <mail@nckn.dk>
2020-08-26 10:55:50 +0200

    Styling PageInfo

--- components/gui/PageInfo.vue
+++ components/gui/PageInfo.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   .sideinfo-container
+    .blocks.tl
+      label.text 3D Type Editor
     .blocks.bl
-      label.text.grey-it Made by #[a(href='https://www.konradstudio.com' target="_blank") Konrad Studio]
+      label.text.grey-it.small-it Made by #[a(href='https://www.konradstudio.com' target="_blank") Konrad Studio]
 </template>
 
 <script>
@@ -31,6 +33,15 @@ export default {
 
 // .sideinfo-container { background: red; }
 
+$breakp-xs: 480px;
+$breakp-sm: 576px;
+$breakp-md: 768px;
+$breakp-lg: 992px;
+$breakp-xl: 1200px;
+$breakp-xxl: 1600px;
+
+$uni-margin: 16px;
+
 .sideinfo-container {
   width: 100%; 
   height: 100%;
@@ -43,9 +54,16 @@ export default {
     a {
       pointer-events: all;
     }
+    &.tl {
+      left: $uni-margin;
+      top: $uni-margin;
+    }
     &.bl {
-      left: 24px;
-      bottom: 24px;
+      left: $uni-margin;
+      bottom: $uni-margin;
+      @media screen and (max-width: $breakp-md) {
+        bottom: 64px;
+      }
     }
     &.center {
       
